2c7a27a5f6f57fe3d45c98eb06467789b1f4ea95
Ngoc Dao <ngocdaothanh@gmail.com>
2019-11-01 14:43:03 +1100

    Split PictureRecorder out to a separate file

--- Sources/SkiaSwift/Picture.swift
+++ Sources/SkiaSwift/Picture.swift
@@ -1,33 +1,5 @@
 import CSkia
 
-public class PictureRecorder {
-  static func new() -> PictureRecorder {
-    let raw = sk_picture_recorder_new()
-    return PictureRecorder(raw!)
-  }
-
-  var raw: OpaquePointer
-
-  init(_ raw: OpaquePointer) {
-    self.raw = raw
-  }
-
-  deinit {
-    sk_picture_recorder_delete(raw)
-  }
-
-  public func beginRecording(rect: Rect) -> Canvas {
-    var r = rect.toSk()
-    let rawCanvas = sk_picture_recorder_begin_recording(raw, &r)
-    return Canvas(raw: rawCanvas!)
-  }
-
-  public func endRecording() -> Picture {
-    let rawPicture = sk_picture_recorder_end_recording(raw)
-    return Picture(rawPicture!)
-  }
-}
-
 public class Picture {
   var raw: OpaquePointer
 

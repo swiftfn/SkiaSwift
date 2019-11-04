f3f1443bdea211cfa7ed5693394eee4d70a9b263
Ngoc Dao <ngocdaothanh@gmail.com>
2019-11-05 10:57:29 +1100

    Indent with 2 spaces

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -5,45 +5,45 @@ import SkiaSwift
 import Foundation
 
 func makeSurface(_ w: Int32, _ h: Int32) -> Surface {
-    let info = ImageInfo(w, h, .rgba8888, .premul)
-    return Surface.raster(info)
+  let info = ImageInfo(w, h, .rgba8888, .premul)
+  return Surface.raster(info)
 }
 
 func emitPng(_ path: String, _ surface: Surface) {
-    let image = surface.snapshot
-    let encoded = image.encode()
-    let bytes = encoded.data
-    let size = encoded.size
-    let data = Data(bytes: bytes, count: size)
-    try? data.write(to: URL(fileURLWithPath: path))
+  let image = surface.snapshot
+  let encoded = image.encode()
+  let bytes = encoded.data
+  let size = encoded.size
+  let data = Data(bytes: bytes, count: size)
+  try? data.write(to: URL(fileURLWithPath: path))
 }
 
 func draw(_ canvas: Canvas) {
-    let fill = Paint()
-    fill.color = Color(r: 0, g: 0, b: 0xFF)
-    canvas.drawPaint(fill)
+  let fill = Paint()
+  fill.color = Color(r: 0, g: 0, b: 0xFF)
+  canvas.drawPaint(fill)
 
-    fill.color = Color(r: 0, g: 0xFF, b: 0xFF)
-    let rect = Rect(100.0, 100.0, 540.0, 380.0)
-    canvas.drawRect(rect, fill)
+  fill.color = Color(r: 0, g: 0xFF, b: 0xFF)
+  let rect = Rect(100.0, 100.0, 540.0, 380.0)
+  canvas.drawRect(rect, fill)
 
-    let stroke = Paint()
-    stroke.color = Color(r: 0xFF, g: 0, b: 0)
-    stroke.antialias = true
-    stroke.stroke = true
-    stroke.strokeWidth = 5.0
+  let stroke = Paint()
+  stroke.color = Color(r: 0xFF, g: 0, b: 0)
+  stroke.antialias = true
+  stroke.stroke = true
+  stroke.strokeWidth = 5.0
 
-    let path = Path()
-    path
-        .moveTo(50.0, 50.0)
-        .lineTo(590.0, 50.0)
-        .cubicTo(-490.0, 50.0, 1130.0, 430.0, 50.0, 430.0)
-        .lineTo(590.0, 430.0)
-    canvas.drawPath(path, stroke)
+  let path = Path()
+  path
+    .moveTo(50.0, 50.0)
+    .lineTo(590.0, 50.0)
+    .cubicTo(-490.0, 50.0, 1130.0, 430.0, 50.0, 430.0)
+    .lineTo(590.0, 430.0)
+  canvas.drawPath(path, stroke)
 
-    fill.color = Color(r: 0, g: 0xFF, b: 0, a: 0x80)
-    let rect2 = Rect(120.0, 120.0, 520.0, 360.0)
-    canvas.drawOval(rect2, fill)
+  fill.color = Color(r: 0, g: 0xFF, b: 0, a: 0x80)
+  let rect2 = Rect(120.0, 120.0, 520.0, 360.0)
+  canvas.drawOval(rect2, fill)
 }
 
 let surface = makeSurface(640, 480)
